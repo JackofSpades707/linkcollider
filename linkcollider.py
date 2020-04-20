0d4b95d4e0f3bd13b53a2aad2b6e3c81343246fe
jackofspades707 <jackofspades707@hotmail.com>
2020-04-19 17:24:47 -0700

    refactored 2 or 3 year old code

--- linkcollider.py
+++ linkcollider.py
@@ -9,32 +9,33 @@ import argparse
 import threading
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--threads', type=int, default=100, help='max number of threads to run, 1 account runs for each thread')
+parser.add_argument('-v', '--verbose', default=False, action='store_true', help='verbose output')
 args = parser.parse_args()
 
-class Bot:
+class Bot(threading.Thread):
     def __init__(self, email, password, proxy=None):
         self._email = email
         self._password = password
-        self._proxy = self.setup_proxy(proxy)
-        self._headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36'}
+        self._proxy = self.setup_proxy(proxy.strip())
+        self._headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
         self._session = requests.Session()
         self._points = 0
         self._running = True
+        threading.Thread.__init__(self)
 
     def setup_proxy(self, proxy):
-        if proxy is None:
+        if proxy is None or proxy == '' or proxy.lower() == 'none':
             return None
         return {'http': 'http://{}'.format(proxy), 'https': 'https://{}'.format(proxy)}
 
     def output(self, s):
-        print("{}: {}").format(self._email, s)
+        print("{}: {}".format(self._email, s))
 
     def login(self):
         payload = {'email': self._email, 'pw': self._password, "remember": "on", 'reqlogin': ''}
         self._session.get("https://www.linkcollider.com/page/login", headers=self._headers, proxies=self._proxy)
         self._session.post("https://www.linkcollider.com/action/login", payload, headers=self._headers, proxies=self._proxy)
-        self.output('Logged In')
+        self.output('{} Logged In'.format(self._email))
 
     def autosurf(self):
         for i in range(0, 20):
@@ -61,7 +62,7 @@ class Bot:
                 return
 
     def check_points(self, source):
-        return BeautifulSoup(source.content, "html.parser").find(class_="color-red rtoken").get_text()
+        return BeautifulSoup(source.content, "html.parser").find(class_="color-gold").get_text()
 
     def data_identifier(self, i, source, selector): # i valid values are 0-19
         try:
@@ -74,12 +75,12 @@ class Bot:
         except IndexError:
             return None
         if args.verbose and selector == 'url':
-            self.output("key: {}").format(key)
-            self.output("uid: {}").format(uid)
-            self.output("posts: {}").format(posts)
-            self.output("posts_reward: {}").format(posts_reward)
-            self.output("posts_noref: {}").format(posts_noref)
-            self.output("postsss: {}").format(postsss)
+            self.output("key: {}".format(key))
+            self.output("uid: {}".format(uid))
+            self.output("posts: {}".format(posts))
+            self.output("posts_reward: {}".format(posts_reward))
+            self.output("posts_noref: {}".format(posts_noref))
+            self.output("postsss: {}".format(postsss))
         if selector == 'url':
             built_url = "http://www.linkcollider.com/page/singlesurf/{0}/autosurf/{1}/{2}/{3}/?uid={4}&ss={5}".format(posts, key, posts_reward, posts_noref, uid, postsss)
             return built_url
@@ -93,39 +94,14 @@ class Bot:
         while self._running is True:
             self.autosurf()
 
-def make_threads(accounts, proxies):
-    q = []
-    for n, account in enumerate(accounts):
-        account = "{}|{}".format(account, proxies[n])
-        q.append(account)
-    threads = []
-    for i in range(args.threads):
-        account = q[i].split('|')
-        email = account[0]
-        password = account[1]
-        proxy = account[2]
-        bot = Bot(email, password, proxy)
-        t = threading.Thread(target=bot.run)
-        threads.append(t)
-    return threads
-
-def run_thread(threads):
-    t = threads[0]
-    del(threads[0])
-    t.run()
-
 def Main():
     with open('accounts.txt') as f:
         accounts = f.readlines()
-    with open('proxies.txt') as f:
-        proxies = f.readlines()
-    threads = make_threads(accounts, proxies)
-    while len(threads) >= 1:
-        if threading.active_count() < args.threads:
-            run_thread(threads)
-        time.sleep(0.5)
+    for account in accounts:
+        email, password, proxy = account.split('|')
+        proxy = proxy.strip()
+        Bot(email, password, proxy).start()
 
 
 if __name__ == "__main__":
     Main()
-
